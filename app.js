7f7e7dec91f674576be5397d1307a7e5c62c57de
Freddy Marquez <freddymarquez@Freddys-MacBook-Pro.local>
2018-04-12 21:22:53 -0500

    forgot to delete localhost

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, "localhost", function(){
+app.listen(process.env.PORT, function(){
   console.log("YelpCamp App Started...")
 });
